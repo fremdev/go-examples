f6cd46cc9009c295c404dcf7ea6c10c21e521059
Alex Efremov <alex.efremov.dev@gmail.com>
2017-11-26 19:28:48 +0200

    Add tests for save to file and load from file

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "os"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +17,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Ten of Clubs" {
 		t.Errorf("Expected Ten of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Expected length 20, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
