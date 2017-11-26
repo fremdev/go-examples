4f183d654353933b825c805a5c64e4c0c6fe8ff1
Alex Efremov <alex.efremov.dev@gmail.com>
2017-11-26 18:03:04 +0200

    Add shuffle functionality

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,10 @@
 package main
 
 func main() {
-	// cards := newDeck()
+	cards := newDeck()
 
 	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
