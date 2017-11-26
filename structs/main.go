36253417112714e7540e33d5907fc4771ba1731b
Alex Efremov <alex.efremov.dev@gmail.com>
2017-11-26 22:53:25 +0200

    Add structs folder woth basic structs examples

---
+++ structs/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contacts struct {
+	zipCode int
+	email   string
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contacts
+}
+
+func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contacts: contacts{
+			zipCode: 45300,
+			email:   "alex@vikings.com",
+		},
+	}
+	alex.print()
+
+	var bob person
+	bob.firstName = "Bob"
+	bob.lastName = "King"
+	bob.contacts = contacts{email: "bob@gmail.com", zipCode: 90210}
+	bob.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
