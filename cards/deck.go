d63f8fa23acfc0f3901bf41b9196b60131e182af
Alex Efremov <alex.efremov.dev@gmail.com>
2017-11-26 15:31:10 +0200

    Load new deck from file

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,4 +40,15 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0664)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error from newDeckFromFile:", err)
+		os.Exit(1) // Exit from the program
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
